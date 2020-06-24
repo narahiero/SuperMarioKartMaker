5ce4035f74a08786a2e93ef9aef976b7bd222c92
Nara Hiero <narahiero@gmail.com>
2020-06-24 11:54:51 -0400

    Add activeProjectChanged listener to Outliner

--- Source/UI/Editor/SMKM/Outliner.cpp
+++ Source/UI/Editor/SMKM/Outliner.cpp
@@ -12,18 +12,28 @@
 #include <QTreeWidget>
 
 #include "Core/Core.hpp"
+#include "Core/Project.hpp"
 
+#include "UI/CoreCallbacks.hpp"
 #include "UI/Editor/SMKM/Editors.hpp"
 
 Outliner::Outliner(EditorContainer* container) :
-    AbstractEditor(container, Editors::Outliner)
+    AbstractEditor(container, Editors::Outliner),
+    m_hasProject{Core::hasActiveProject()}
 {
     createLayout();
     createNoProjectLabel();
     createTree();
+
+    connect(CoreCallbacks::instance(), &CoreCallbacks::activeProjectChanged,
+        this, &Outliner::activeProjectChanged);
 }
 
-Outliner::~Outliner() = default;
+Outliner::~Outliner()
+{
+    m_noProject->deleteLater();
+    m_tree->deleteLater();
+}
 
 void Outliner::createLayout()
 {
@@ -38,7 +48,7 @@ void Outliner::createNoProjectLabel()
     m_noProject = new QLabel(tr("No active project to show outline from..."));
     m_noProject->setWordWrap(true);
 
-    if (!Core::hasActiveProject())
+    if (!m_hasProject)
     {
         m_layout->addWidget(m_noProject, 1, Qt::AlignCenter);
     }
@@ -50,13 +60,61 @@ void Outliner::createTree()
     m_tree->setHeaderHidden(true);
     m_tree->setColumnCount(1);
 
-    if (Core::hasActiveProject())
+    if (m_hasProject)
     {
         m_layout->addWidget(m_tree, 1);
+
+        m_projectNode = new OutlinerNode(Core::activeProject()->name(), m_tree);
     }
 }
 
 AbstractEditor* Outliner::duplicate() const
 {
     return new Outliner(container());
+}
+
+void Outliner::activeProjectChanged(const std::shared_ptr<Project>& project)
+{
+    if (project)
+    {
+        if (!m_hasProject)
+        {
+            m_noProject->setParent(nullptr);
+            m_layout->addWidget(m_tree, 1);
+            m_hasProject = true;
+        }
+
+        m_tree->clear();
+        m_projectNode = new OutlinerNode(project->name(), m_tree);
+    }
+    else
+    {
+        if (m_hasProject)
+        {
+            m_tree->setParent(nullptr);
+            m_layout->addWidget(m_noProject, 1, Qt::AlignCenter);
+            m_hasProject = false;
+        }
+    }
+}
+
+////// OutlinerNode class //////////////
+
+OutlinerNode::OutlinerNode(Property<std::string>& name, QTreeWidget* parent) :
+    QTreeWidgetItem(parent),
+    PropertyObserver(name)
+{
+    setText(0, QString::fromStdString(name.get()));
+
+    if (parent != nullptr)
+    {
+        setExpanded(true);
+    }
+}
+
+OutlinerNode::~OutlinerNode() = default;
+
+void OutlinerNode::valueUpdated(const std::string& name)
+{
+    setText(0, QString::fromStdString(name));
 }
--- Source/UI/Editor/SMKM/Outliner.hpp
+++ Source/UI/Editor/SMKM/Outliner.hpp
@@ -9,9 +9,16 @@
 
 #include "UI/Editor/AbstractEditor.hpp"
 
+#include <QTreeWidget>
+
+#include "Common/Property.hpp"
+
 class QLabel;
-class QTreeWidget;
 class QVBoxLayout;
+
+class Project;
+
+class OutlinerNode;
 
 class Outliner final : public AbstractEditor
 {
@@ -34,9 +41,28 @@ private:
     void createNoProjectLabel();
     void createTree();
 
+    // slots
+
+    void activeProjectChanged(const std::shared_ptr<Project>& project);
+
     // fields
 
     QVBoxLayout* m_layout;
+    bool m_hasProject;
+
     QLabel* m_noProject;
+
     QTreeWidget* m_tree;
+    OutlinerNode* m_projectNode;
+};
+
+class OutlinerNode final : public QTreeWidgetItem, public PropertyObserver<std::string>
+{
+
+public:
+
+    explicit OutlinerNode(Property<std::string>& name, QTreeWidget* parent = nullptr);
+    ~OutlinerNode();
+
+    void valueUpdated(const std::string& name) override;
 };
